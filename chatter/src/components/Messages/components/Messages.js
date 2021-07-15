d7c9f0e89316684a5435846d864b077ea8087a57
mahady-manana <rm.mahady@gmail.com>
2021-07-15 21:00:09 +0300

    Chatter

--- chatter/src/components/Messages/components/Messages.js
+++ chatter/src/components/Messages/components/Messages.js
@@ -1,32 +1,81 @@
-import React, { useContext } from 'react';
-import io from 'socket.io-client';
-import useSound from 'use-sound';
-import config from '../../../config';
-import LatestMessagesContext from '../../../contexts/LatestMessages/LatestMessages';
-import TypingMessage from './TypingMessage';
-import Header from './Header';
-import Footer from './Footer';
-import Message from './Message';
-import '../styles/_messages.scss';
+import React, { useContext, useState, useEffect, useCallback } from "react";
+import io from "socket.io-client";
+import useSound from "use-sound";
+import config from "../../../config";
+import LatestMessagesContext from "../../../contexts/LatestMessages/LatestMessages";
+import TypingMessage from "./TypingMessage";
+import Header from "./Header";
+import Footer from "./Footer";
+import Message from "./Message";
+import "../styles/_messages.scss";
+import INITBOTTYMESSAGE from "../../../common/constants/initialBottyMessage";
 
-const socket = io(
-  config.BOT_SERVER_ENDPOINT,
-  { transports: ['websocket', 'polling', 'flashsocket'] }
-);
-
+const socket = io(config.BOT_SERVER_ENDPOINT, {
+	transports: ["websocket", "polling", "flashsocket"],
+});
+const INITILMESSAGE = {
+	message: INITBOTTYMESSAGE,
+	id: Date.now(),
+	user: "bot",
+};
 function Messages() {
-  const [playSend] = useSound(config.SEND_AUDIO_URL);
-  const [playReceive] = useSound(config.RECEIVE_AUDIO_URL);
-  const { setLatestMessage } = useContext(LatestMessagesContext);
+	const [myMessage, setMyMessage] = useState("");
+	const [messageLists, setMessageLists] = useState([INITILMESSAGE]);
+	const [isBottyTyping, setIsBottyTyping] = useState(false);
+	const [playSend] = useSound(config.SEND_AUDIO_URL);
+	const [playReceive] = useSound(config.RECEIVE_AUDIO_URL);
+	const { setLatestMessage } = useContext(LatestMessagesContext);
 
-  return (
-    <div className="messages">
-      <Header />
-      <div className="messages__list" id="message-list">
-      </div>
-      <Footer message={message} sendMessage={sendMessage} onChangeMessage={onChangeMessage} />
-    </div>
-  );
+	useEffect(() => {
+		socket.off("bot-message");
+		socket.on("bot-message", (bottyMessage) => {
+			setMessageLists([
+				...messageLists,
+				{ message: bottyMessage, user: "bot", id: Date.now() },
+			]);
+			setLatestMessage("bot", bottyMessage);
+			playReceive();
+		});
+		return () => {};
+	}, [messageLists, playReceive]);
+
+	useEffect(() => {
+		document.getElementById("user-message-input").focus();
+
+		socket.on("bot-typing", () => {
+			setIsBottyTyping(true);
+		});
+	}, [isBottyTyping]);
+
+	const sendMessage = useCallback(() => {
+		setMessageLists([
+			...messageLists,
+			{ message: myMessage, id: Date.now(), user: "IAM" },
+		]);
+		setLatestMessage("IAM", myMessage);
+		playSend();
+		socket.emit("user-message", myMessage);
+		document.getElementById("user-message-input").value = "";
+	}, [messageLists, myMessage, playSend, setLatestMessage]);
+	const handleChange = (event) => {
+		setMyMessage(event.target.value);
+	};
+	return (
+		<div className='messages'>
+			<Header />
+			<div className='messages__list' id='message-list'>
+				{messageLists.map((mes, index) => {
+					return <Message message={mes} botTyping={isBottyTyping} />;
+				})}
+				{isBottyTyping && <TypingMessage />}
+			</div>
+			<Footer
+				message={myMessage}
+				sendMessage={sendMessage}
+				onChangeMessage={handleChange}
+			/>
+		</div>
+	);
 }
 
 export default Messages;
